604030c34784c7bb233416c9d854c415265b20ae
Lawrence <lawrence@edu.ink>
2020-10-21 00:40:08 +0800

    Updates

---
+++ precache-manifest.c1f543033bfb7cf263e2891bb9e08848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7d43c68fd179680ae9b189cc8b55ba",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "fd444b4152ef69279271",
+    "url": "/todo-app/static/css/main.81720426.chunk.css"
+  },
+  {
+    "revision": "cc806112122a48a00f67",
+    "url": "/todo-app/static/js/2.9227f636.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-app/static/js/2.9227f636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd444b4152ef69279271",
+    "url": "/todo-app/static/js/main.01f16aab.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
